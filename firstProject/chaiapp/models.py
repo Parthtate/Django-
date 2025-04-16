c0075fdc8dc3fd31e3ecc31a558f8d519d5505e0
Parthtate <parthtate575@gmail.com>
2025-04-16 10:20:37 +0530

    add chaiapp and fix bugs

--- firstProject/chaiapp/models.py
+++ firstProject/chaiapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -22,3 +23,38 @@ class ChaiVarity(models.Model):
     def __str__(self):
         return self.name
     
+
+# one to many relation
+
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name="review")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} review on the {self.chai.name}"
+    
+# Many to many 
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varities = models.ManyToManyField(ChaiVarity, related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+# One to one 
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=50)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Certificate for {self.name.chai}"
+    
+
--- firstProject/chaiapp/views.py
+++ firstProject/chaiapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import ChaiVarity
+from .models import ChaiVarity, Store
+from .forms import ChaiVarityForm
 
 # Create your views here.
 # Step1: before define methods, go to settings and go into the INSTALLEAPPS and add chaiapp
@@ -7,3 +8,14 @@ from .models import ChaiVarity
 def all_chai(request):
     chais = ChaiVarity.objects.all()
     return render(request, 'chaiapp/all_chai.html', {'chais': chais})
+
+def store(request):
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarityForm(request.POST)
+        if form.is_valid():
+            chai_variety = form.cleaned_data['chai_variety']
+            stores = Store.objects.filter(chai_varities=chai_variety)
+    else: 
+        form = ChaiVarityForm()        
+    return render(request, 'chaiapp/store.html', {'stores': stores, 'form':form})
